efbaab32d528bb2e087d2fea20c9f7687fabd167
Ruto7708 <117638848+Ruto7708@users.noreply.github.com>
2025-04-02 17:15:41 +0300

    Update wsgi.py

--- Rusonik/wsgi.py
+++ Rusonik/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rusonik-Loans-Application.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rusonik.settings')
 
 application = get_wsgi_application()
