6c684f57c7427809971fbd2b62985059e3acd5f7
Ruto7708 <117638848+Ruto7708@users.noreply.github.com>
2025-04-03 11:20:40 +0300

    Update settings.py

--- Rusonik/settings.py
+++ Rusonik/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'Rusonik.wsgi.application'
 # postgresql://rusonikdb_user:LVt4Z24aYaq7y6JnPcaVaHASNlJg931n@dpg-cvn3ecc9c44c73digl6g-a.oregon-postgres.render.com/rusonikdb
 
 DATABASES = {
-    'default': dj_database_url.parse("postgresql://rusonikdb_user:LVt4Z24aYaq7y6JnPcaVaHASNlJg931n@dpg-cvn3ecc9c44c73digl6g-a.oregon-postgres.render.com/rusonikdb")
+    'default': dj_database_url.parse("postgresql://rusonikdb_user:LVt4Z24aYaq7y6JnPcaVaHASNlJg931n@dpg-cvn3ecc9c44c73digl6g-a/rusonikdb")
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
